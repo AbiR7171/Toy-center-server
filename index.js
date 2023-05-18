184afe897e8f2601676e8f8ee2aa5c6d09f6ef95
AbiR7171 <abiremtiag@gmail.com>
2023-05-18 15:15:22 +0600

    Conneted to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express')
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000 
 
 app.use(cors())
 app.use(express.json())
+
+// Toy-center
+// lc9koLm8mKrgztq7
+
+
+
+const uri = `mongodb+srv://${process.env.Mongo_USER}:${process.env.Mongo_PASS}@cluster0.hmmbger.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res)=>{
     res.send("Toy-center server is running")
